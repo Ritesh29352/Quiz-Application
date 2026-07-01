4a04da67731394c12c98ce6aa5a71d57503b81f9
Ritesh29352 <kumarritesh29352@gmail.com>
2026-07-01 17:56:55 +0530

    changed db password

--- src/main/java/org/example/db/DBConnection.java
+++ src/main/java/org/example/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3307/quiz_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Windows@352";
+    private static final String PASSWORD = "Yourpassword";
 
     public static Connection getConnection() {
 
